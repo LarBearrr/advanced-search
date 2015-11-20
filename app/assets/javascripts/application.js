3502c4e1dd1f99ce7acf55745c17180aadfc720c
Paul Arterburn <paularterburn@gmail.com>
2015-11-20 11:36:33 -0700

    Allow ENV overrides

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,27 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap-sprockets
 //= require jquery_ujs
-//= require turbolinks
+//= require jquery.autosize
 //= require_tree .
+
+$(document).ready(function(){
+    // grow text area based on content
+    $('textarea').autosize();
+
+    // remove empty line breaks from textrea
+    $('textarea').blur(function(){
+        var value = $(this).val();
+        value=value.replace(/^\s*[\r\n]/gm,'');
+        $(this).val(value);
+        $('textarea').trigger('autosize.resize');
+    });
+
+    // show indicator as it's querying
+    $('#j-brandfolder-advanced-search').submit(function(e) {
+        $("#submit_search").attr('disabled', true);
+        $("#submit_search").find('span').hide();
+        $("#submit_search").find('.la-ball-fall').show();
+    });
+});
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,34 @@
 class WelcomeController < ApplicationController
   def query
-    token = ENV['BF_TOKEN'] || params[:bf_token]
-    slug = ENV['BF_SLUG'] || (params[:slug].split('/').last.strip if params[:slug].present?)
-    if token.present? && slug.present?
-      queries = params[:query].split.uniq
-      render json: { url: 'https://brandfolder.com/' }, status: :ok
+    @token = ENV['BF_API_KEY'] || params[:bf_api_key]
+    @slug = params[:bf_url].split('/').last.strip if params[:bf_url].present?
+    @queries = params[:bf_query].split(/\r?\n/).uniq.reject(&:empty?)
+    if @token.present? && @slug.present? && @queries.present?
+      redirect_url = get_manifest_url
+      if redirect_url.present?
+        redirect_to 'https://brandfolder.com/agency'
+      else
+        flash.now[:warning] = 'No assets matched queries.'
+        render 'index'
+      end
     else
-      render json: { message: 'Please fill out all fields.' }, status: 422
+      flash.now[:danger] = 'Please fill out all the fields.'
+      render 'index'
     end
+  end
+
+  private
+
+  def get_manifest_url
+    # uri = URI.parse("https://brandfolder.com/api/#{@slug}/qa/assets/advanced_search.json?token=#{@token}")
+    # http = Net::HTTP.new(uri.host, uri.port)
+    # http.use_ssl = true
+    # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    # request = Net::HTTP::Post.new('/v1.1/auth')
+    # request.add_field('Content-Type', 'application/json')
+    # request.body = {'queries' => @queries}
+    # response = http.request(request)
+    # 'https://brandfolder.com/agency'
+    ''
   end
 end
