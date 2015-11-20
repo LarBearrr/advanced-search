b2d1178e93c5ebfd37d57e1f7af51dd95b6ad648
Paul Arterburn <paularterburn@gmail.com>
2015-11-19 18:21:14 -0700

    Example

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,12 @@
 class WelcomeController < ApplicationController
+  def query
+    token = ENV['BF_TOKEN'] || params[:bf_token]
+    slug = ENV['BF_SLUG'] || (params[:slug].split('/').last.strip if params[:slug].present?)
+    if token.present? && slug.present?
+      queries = params[:query].split.uniq
+      render json: { url: 'https://brandfolder.com/' }, status: :ok
+    else
+      render json: { message: 'Please fill out all fields.' }, status: 422
+    end
+  end
 end
